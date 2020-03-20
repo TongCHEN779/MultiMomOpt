c67e6b87e9a5291ed3ee234cb558f115f3dd0e88
Tong Chen <tchen@laas.fr>
2020-03-20 19:33:45 +0100

    add code

--- src_julia/MultiMomOpt.jl
+++ src_julia/MultiMomOpt.jl
@@ -12,7 +12,7 @@ using MosekTools
 using Printf
 using TSSOS
 
-export chordal_extension, get_basis, basis2supp, pol2supp, MomentMatrix, LocalizationMatrix, solve_moment_auto, solve_moment_manual, solve_moment_maxcut, solve_moment_mip
+export chordal_extension, get_basis, basis2supp, pol2supp, MomentMatrix, LocalizationMatrix, solve_moment_auto, solve_moment_manual, solve_moment_maxcut, solve_moment_mip, solve_moment_lip_two_layer, solve_moment_lip_one_layer_tssos, solve_moment_lip_one_layer
 
 include("basic_functions.jl")
 include("solve_moment.jl")
