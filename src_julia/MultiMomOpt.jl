80bed0761c33d2369bbb94011e9571cf2a5a9a29
Tong Chen <tchen@laas.fr>
2020-03-20 19:13:58 +0100

    add code

--- src_julia/MultiMomOpt.jl
+++ src_julia/MultiMomOpt.jl
@@ -10,6 +10,7 @@ using GraphPlot
 using DynamicPolynomials
 using MosekTools
 using Printf
+using TSSOS
 
 export chordal_extension, get_basis, basis2supp, pol2supp, MomentMatrix, LocalizationMatrix, solve_moment_auto, solve_moment_manual, solve_moment_maxcut, solve_moment_mip
 
